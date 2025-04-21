fc5be40929439c238f1c183f9126574abe4f81cf
AlterShot <Vigglex@mail.ru>
2025-04-21 12:07:09 +0300

    Обновлено

--- chucknorris.py
+++ chucknorris.py
@@ -13,35 +13,42 @@ class ChuckNorrisJoke():
         category_url = self.url + category_list_path
 
         # Печатаем путь
-        print(category_url)
+        print("\n----" * 2)
+        print(f"\U0001F517path to categories: {category_url}")
+        print("\n----" * 2)
 
         # Собираем все категории и выводим их
         category_list = requests.get(category_url).json()
-        print(f"all categories: {category_list}")
+        print("\n----" * 2)
+        print(f"\U0001F4DAall categories: ")
+        for category in category_list:
+            print(f" - {category}")
+        print("\n----" * 2)
 
         # Проходимся по списку полученных категорий
         for category in category_list:
-
+            print("\n----" * 2)
             # Подставляем значения в url для запроса get
             many_jokes_path = f"/jokes/random?category={category}"
             many_jokes_url = self.url + many_jokes_path
 
             # Печатаем полученные url
-            print(many_jokes_url)
+            print(f"\n\U0001F50Dpath to current joke: {many_jokes_url}")
 
             # Выводим на печать то, что получилось
             show_jokes = requests.get(many_jokes_url)
             result = show_jokes.json()
-            print(result)
+            print(f"\n\U0001F4E6full json info: {result}")
 
             # Проверяем статус-код для каждой шутки
-            print(f"status code: {show_jokes.status_code}")
-            assert show_jokes.status_code == 200, "status code wrong"
-            print("status code correct")
+            print(f"\n\u26A0current path status code: {show_jokes.status_code}")
+            assert show_jokes.status_code == 200, "\u274Cstatus code wrong"
+            print("\n\u2705status code check correct")
 
             # Выводим каждую шутку отдельно
             joke_only = result.get("value")
-            print(joke_only)
+            print(f"\n\U0001F602and the joke by itself is: {joke_only}")
+            print("\n----" * 2)
 
 
 # Создаем экземпляр класса и вызываем метод
