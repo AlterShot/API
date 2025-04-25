d124bcd1063b511585e78069a498a302d2ef18ef
AlterShot <Vigglex@mail.ru>
2025-04-25 16:05:52 +0300

    Обновлено

--- maps_hw.py
+++ maps_hw.py
@@ -9,10 +9,11 @@ class MapsPostNGet():
     key = '?key=qaclick123'
     post_path = '/maps/api/place/add/json'
     get_path = '/maps/api/place/get/json'
+    delete_path = '/maps/api/place/delete/json'
 
     location_description = {"location": {
-        "latitude": "-38.383494",
-        "longitude": "33.427362"},
+        "lat": "-38.383494",
+        "lng": "33.427362"},
         "accuracy": "50",
         "name": "Frontline house",
         "phone_number": "(+91) 983 893 3937",
@@ -59,11 +60,11 @@ class MapsPostNGet():
 
         # Достаем id из файла
         with open('places.txt', 'r') as places_to_read:
-            place_id = [line.strip() for line in places_to_read]
+            place_ids = [line.strip() for line in places_to_read]
 
         # Подставляем id из файла в GET запрос
-        for places in place_id:
-            full_get_url = get_url + f"{places}"
+        for place_id in place_ids:
+            full_get_url = get_url + f"{place_id}"
             process_of_get = requests.get(full_get_url)
 
             # Проверяем, не пустой ли id
@@ -77,8 +78,61 @@ class MapsPostNGet():
             assert process_of_get.status_code == 200, "status code in GET is wrong"
             print("status code GET correct")
 
+    # Создаем метод для удаления
+    def delete_stuff(self) -> None:
+        # Перебираем id
+        with open('places.txt', 'r') as places_to_delete:
+            place_ids = [line.strip() for line in places_to_delete.readlines()]
+
+        # Создаем переменную для удаления
+        delete_ids = [place_ids[1], place_ids[3]]
+
+        # Удаляем 2 и 4 id из списка
+        for place_id in delete_ids:
+            delete_url = self.base_url + self.delete_path + self.key
+            deletion = {"place_id": place_id}
+            delete_process = requests.delete(delete_url, json=deletion)
+
+            # Проверяем статус-код запроса и печатаем его
+            print(f"DELETE status code: {delete_process.status_code}")
+            assert delete_process.status_code == 200, f"DELETE status code {place_id} is wrong"
+            print(f"DELETE status code {place_id} correct")
+
+    # Создаем метод для получения нового списка
+    def get_new(self) -> None:
+        # Собираем url
+        get_url = self.base_url + self.get_path + self.key + "&place_id="
+
+        # Перебираем список id
+        with open('places.txt', 'r') as places_to_read:
+            place_ids = [line.strip() for line in places_to_read.readlines()]
+
+        # Создаем переменную для хранения существующих id
+        places_exist = []
+
+        # Перебираем id и делаем запросы
+        for place_id in place_ids:
+            getting_ids = requests.get(get_url + place_id)
+
+            # Проверяем статус-коды запросов
+            if getting_ids.status_code == 200 and getting_ids.json().get('name'):
+                places_exist.append(place_id)
+                print(f"{place_id} correct")
+            else:
+                print(f"{place_id} wrong")
+
+        # Записываем id в новый файл
+        with open('existing_places.txt', 'w') as file:
+            for place_id in places_exist:
+                file.write(place_id + "\n")
+        print("IDs added")
+
 
 # Создаем экземпляр класса и вызывает методы
 start = MapsPostNGet()
+print("test start")
 start.post_and_write()
 start.get_and_read()
+start.delete_stuff()
+start.get_new()
+print("test finish")
